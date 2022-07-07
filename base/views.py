28fc8edc0c1e64a02b8d5bf5b0f80106b7375b0f
Success Divine Onyechinemere <divinesuccess228@gmail.com>
2022-07-07 04:20:28 +0100

    views: add register function ,  start updating room function . template: upate login_register.html, navbar.html and room.html

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
@@ -18,11 +19,12 @@ from .forms import RoomForm
     
 def loginPage(request):
 
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
-        
+
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -36,19 +38,36 @@ def loginPage(request):
             login(request, user)
             return redirect('home')
         else:
-            messages.error(request, 'Username of Password does not exist')
+            messages.error(request, 'Username or Password does not exist')
 
-    context = {}
+    context = {'page': page}
     return render (request, 'base/login_register.html', context )
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
 
+
+def registerUser(request):
+    form = UserCreationForm()
+    if request.method ==  'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) #collects data but allows for further checking
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+    return render(request, 'base/login_register.html', {'form': form})
+
+
 def home (request):
+    #q is for query
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
-        Q(topic__name__icontains=q) |
+        Q(topic__name__icontains=q) | #gets the different search methods irrespective of casing
         Q(name__icontains=q) |
         Q(description__icontains=q)
         )
@@ -60,7 +79,8 @@ def home (request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    messages = room.message_set.all()
+    context = {'room': room, 'messages':messages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url= 'login')
